e57de3cfc912ef8ee0398f6d9c0a89c43ea6fbc0
Fernando Aires <github@friarhob.33mail.com>
2021-07-10 22:29:22 -0300

    Adding sqlite declarations

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///musicas.sqlite3"
+db = SQLAlchemy(app)
+
+class Musica(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(150), nullable=False)
+    artista = db.Column(db.String(150), nullable=False)
+    link = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, nome, artista, link):
+        self.nome = nome
+        self.artista = artista
+        self.link = link
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
